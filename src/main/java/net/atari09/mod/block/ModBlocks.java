5d903cfa5d1d0098105dd329d111cda5ecc37232
atari09modding <atari09.modding@gmail.com>
2025-11-05 16:28:51 +0100

    Changed some things.

--- src/main/java/net/atari09/mod/block/ModBlocks.java
+++ src/main/java/net/atari09/mod/block/ModBlocks.java
@@ -25,9 +25,13 @@ public class ModBlocks {
                     .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
 
     public static final DeferredBlock<Block> BISMUTH_ORE = registerBlock("bismuth_ore",
-            () -> new DropExperienceBlock(UniformInt.of(500,700),
+            () -> new DropExperienceBlock(UniformInt.of(50,70),
                     BlockBehaviour.Properties.of()
                             .strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+    public static final DeferredBlock<Block> BISMUTH_DEEPSLATE_ORE = registerBlock("bismuth_deepslate_ore",
+            () -> new DropExperienceBlock(UniformInt.of(50,70),
+                    BlockBehaviour.Properties.of()
+                            .strength(4f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
 
     private static <T extends Block> DeferredBlock registerBlock(String name, Supplier<T> block) {
