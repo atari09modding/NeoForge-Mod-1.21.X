3720e0e1e782c33d4b2fa6426a71ca09f389129a
atari09modding <atari09.modding@gmail.com>
2025-11-15 14:40:29 +0100

    Changed some things.

--- src/main/java/net/atari09/atarisnewmegamodproject/datagen/ModBlockStateProvider.java
+++ src/main/java/net/atari09/atarisnewmegamodproject/datagen/ModBlockStateProvider.java
@@ -25,12 +25,13 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
         buttonBlock(ModBlocks.BISMUTH_BUTTON.get(), blockTexture(ModBlocks.BISMUTH_BLOCK.get()));
         pressurePlateBlock(ModBlocks.BISMUTH_PRESSURE_PLATE.get(), blockTexture(ModBlocks.BISMUTH_BLOCK.get()));
+
         fenceBlock(ModBlocks.BISMUTH_FENCE.get(), blockTexture(ModBlocks.BISMUTH_BLOCK.get()));
         fenceGateBlock(ModBlocks.BISMUTH_FENCE_GATE.get(), blockTexture(ModBlocks.BISMUTH_BLOCK.get()));
         wallBlock(ModBlocks.BISMUTH_WALL.get(), blockTexture(ModBlocks.BISMUTH_BLOCK.get()));
 
-        doorBlockWithRenderType(ModBlocks.BISMUTH_DOOR.get(), modLoc("blocks/bismuth_door_bottom"), modLoc("blocks/bismuth_door_top"), "cutout");
-        trapdoorBlockWithRenderType(ModBlocks.BISMUTH_TRAPDOOR.get(), modLoc("blocks/bismuth_trapdoor"), true,"cutout");
+        doorBlockWithRenderType(ModBlocks.BISMUTH_DOOR.get(), modLoc("block/bismuth_door_bottom"), modLoc("block/bismuth_door_top"), "cutout");
+        trapdoorBlockWithRenderType(ModBlocks.BISMUTH_TRAPDOOR.get(), modLoc("block/bismuth_trapdoor"), true,"cutout");
 
         blockItem(ModBlocks.BISMUTH_STAIRS);
         blockItem(ModBlocks.BISMUTH_SLAB);
@@ -47,10 +48,10 @@ public class ModBlockStateProvider extends BlockStateProvider {
     }
 
     private void blockItem(DeferredBlock<?> deferredBlock) {
-        simpleBlockItem(deferredBlock.get(), new ModelFile.UncheckedModelFile("tutorialmod:block/" + deferredBlock.getId().getPath()));
+        simpleBlockItem(deferredBlock.get(), new ModelFile.UncheckedModelFile("atarisnewmegamodproject:block/" + deferredBlock.getId().getPath()));
     }
 
     private void blockItem(DeferredBlock<?> deferredBlock, String appendix) {
-        simpleBlockItem(deferredBlock.get(), new ModelFile.UncheckedModelFile("tutorialmod:block/" + deferredBlock.getId().getPath() + appendix));
+        simpleBlockItem(deferredBlock.get(), new ModelFile.UncheckedModelFile("atarisnewmegamodproject:block/" + deferredBlock.getId().getPath() + appendix));
     }
 }
